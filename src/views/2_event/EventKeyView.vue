a700a7b0d99db93ecffc17aebb1e98cecea13dbd
ejlee <ejlee@mediance.co.kr>
2022-12-22 17:46:13 +0900

    이벤트

---
+++ src/views/2_event/EventKeyView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- <input type="search" name="" id="" @keyup="checkEnter($event)" v-model="searchText"> -->
+    <input
+    type="search"
+    name=""
+    id=""
+    @keyup.enter="doSearch"
+    v-model="searchText">
+    <button @click="doSearch">조회</button>
+    <button type="submit" @click.prevent="doSearch"></button>
+  </div>
+</template>
+<script>
+
+// keyup.enter
+// .tab
+// .delete
+// .esc
+// .space
+// .up
+// .down
+// .left
+// .right
+// .stop - event.stopPropagation()
+// .prevent - event.preventDefault()
+
+export default {
+  components: {},
+  data() {
+    return {
+      searchText: ''
+    }
+  },
+  setup() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    doSearch() {
+      console.log(this.searchText)
+    },
+    checkEnter(event) {
+      if (event.keyCode === 13) {
+        this.doSearch()
+      }
+    }
+  }
+}
+</script>
